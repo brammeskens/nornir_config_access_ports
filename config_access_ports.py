76cfb9f2fbdaa8801463298aef9796326f492cf3
Bram Meskens <bram.meskens@gmail.com>
2021-04-01 09:52:57 +0000

    Update config_access_ports.py with comments

--- config_access_ports.py
+++ config_access_ports.py
@@ -13,6 +13,7 @@ from nornir_jinja2.plugins.tasks import template_file
 from nornir_netmiko import netmiko_send_command, netmiko_send_config
 
 def get_access_ports(task):
+    # This task runs the 'show interface switchport' command and makes it machine readable through TextFSM
     r = task.run(
         name = "Getting switchports",
         task = netmiko_send_command,
@@ -20,10 +21,13 @@ def get_access_ports(task):
         use_textfsm = True
     )
 
+    # Create an empty list called 'access_ports' for each host, so we can store our access ports in here
     task.host['access_ports'] = []
+    # Loop through all interfaces gathered in the previous task, save access port information to the list
     for interface in r.result:
         if interface['admin_mode'] == "static access":
             task.host['access_ports'].append(dict(interface))
+    # Print out all the access ports found for the host to make it more visual
     for access_port in task.host['access_ports']:
         rprint(f"[green][{task.host.name}][/green] Interface [blue]{access_port['interface']}[/blue] is of type access")
 
@@ -44,17 +48,23 @@ def push_config(task):
     )
 
 def main():
+    # Set the environment variable so Python can find network-to-code templates folder
     os.environ["NET_TEXTFSM"] = "ntc-templates/templates"
+    # Init Nornir with our config file - You can follow it up with a filter
     nr = InitNornir(config_file="config.yaml")
-
+    
+    # Ask for our users password so we can access the hosts
     nr.inventory.defaults.password = getpass.getpass()
 
+    # Gather all access ports for our devices and print the result
     result_access = nr.run(task = get_access_ports)
     print_result(result_access, vars=["diff","exception"])
 
+    # Generate the configuration for each host, whilst specifying our jinja2 folder and template
     result_gen_config = nr.run(task = generate_config, j2path= "templates/", j2template = "8021x_mon.j2")
     print_result(result_gen_config, vars=["diff","exception"])
 
+    # Push the generated configuration to our hosts
     result_push_config = nr.run(task = push_config)
     print_result(result_push_config, vars=["diff","exception"])
 
